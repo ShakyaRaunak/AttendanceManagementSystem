985dcdba8facd5ea09c3e937aec9a196ec48d616
Raunak Shakya <rkshakya99@gmail.com>
2017-09-24 19:49:00 +0545

    rotate company logo animation for splash screen

--- app/src/main/java/com/naradevi/attendancemanager/LandingActivity.java
+++ app/src/main/java/com/naradevi/attendancemanager/LandingActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -17,14 +20,54 @@ public class LandingActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing);
 
-        new Handler().postDelayed(new Runnable() {
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.antirotate);
+        final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        imageView.startAnimation(animation_2);
+        animation_2.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run() {
-                Intent homeIntent = new Intent(LandingActivity.this, LoginActivity.class);
-                startActivity(homeIntent);
-                finish();
+            public void onAnimationStart(Animation animation) {
+
             }
-        }, SPLASH_TIME_OUT);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        animation_1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_3);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent homeIntent = new Intent(getBaseContext(), LoginActivity.class);
+                        startActivity(homeIntent);
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
 }
